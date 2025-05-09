6776edf1400675e242a6786ca17a7f1d9d2b5091
adam8kac <adam.kac@student.um.si>
2025-05-09 22:09:50 +0200

    s7

--- backend/src/test/java/feri/um/si/omreznina/OmrezninaApplicationTests.java
+++ backend/src/test/java/feri/um/si/omreznina/OmrezninaApplicationTests.java
@@ -1,12 +1,19 @@
 package feri.um.si.omreznina;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import com.google.firebase.internal.FirebaseService;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class OmrezninaApplicationTests {
+
+
+    @Mock
+    private FirebaseService<Object> firebaseService;
 
 	@Test
 	void contextLoads() {
