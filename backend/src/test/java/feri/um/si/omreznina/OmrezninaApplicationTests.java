332ab9fa6638b0be26312938ce2e1af966da608b
adam8kac <adam.kac@student.um.si>
2025-05-09 21:47:00 +0200

    s4

--- backend/src/test/java/feri/um/si/omreznina/OmrezninaApplicationTests.java
+++ backend/src/test/java/feri/um/si/omreznina/OmrezninaApplicationTests.java
@@ -2,8 +2,10 @@ package feri.um.si.omreznina;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("Test")
 class OmrezninaApplicationTests {
 
 	@Test
